aa3e6701349fc073866a8afa0e880ccf74b25938
Giovanna Calderoni <g.c1@me.com>
2017-03-18 23:30:19 -0500

    Deleted Duplicate Files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Movie List</h1>
+        <h1> Movie List </h1>
       </div>
     );
   }
 }
-
-export default App;
